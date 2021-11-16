b9a76d6262f33dca9e3c73fc00d287c42d8d047b
stban2703 <s.t.ban1401@gmail.com>
2021-11-15 23:30:33 -0500

    Saves pokemon in firestore

--- app/src/main/java/com/example/pokedex/PokedexActivity.kt
+++ app/src/main/java/com/example/pokedex/PokedexActivity.kt
@@ -8,6 +8,8 @@ import androidx.lifecycle.lifecycleScope
 import com.example.pokedex.databinding.ActivityPokedexBinding
 import com.example.pokedex.pokedex.Response
 import com.example.pokedex.util.HTTPSWebUtilDomi
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.google.gson.Gson
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -15,7 +17,9 @@ import kotlinx.coroutines.withContext
 
 class PokedexActivity : AppCompatActivity() {
 
+    private val pokedexCollection = Firebase.firestore.collection("pokedex")
     private lateinit var binding: ActivityPokedexBinding
+    private lateinit var user: User
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +27,12 @@ class PokedexActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        // Recuperar informacion del usuario
+        user = intent.extras?.getSerializable("user") as User
+
         binding.catchBtn.setOnClickListener {
-            val pokemonName = binding.catchPokemonET.text.toString().lowercase().filter { !it.isWhitespace() }
+            val pokemonName =
+                binding.catchPokemonET.text.toString().lowercase().filter { !it.isWhitespace() }
             getPokemon(pokemonName)
         }
     }
@@ -38,27 +46,27 @@ class PokedexActivity : AppCompatActivity() {
                 val gson = Gson()
                 val response = gson.fromJson(json, Response::class.java)
 
-                Log.e("Pokemon>>>", response.name)
+                val catchPokemon = Pokemon()
+                catchPokemon.id = response.id
+                catchPokemon.name = response.name
+                catchPokemon.hp = response.stats[0].base_stat
+                catchPokemon.attack = response.stats[1].base_stat
+                catchPokemon.defense = response.stats[2].base_stat
+                catchPokemon.specialAttack = response.stats[3].base_stat
+                catchPokemon.specialDefense = response.stats[4].base_stat
+                catchPokemon.speed = response.stats[5].base_stat
+                catchPokemon.sprite = response.sprites.front_default
 
-                Log.e("Pokemon>>>", response.stats[0].stat.name)
-                Log.e("Pokemon>>>", response.stats[0].base_stat.toString())
+                pokedexCollection.document(user.id).collection("pokedex")
+                    .document(catchPokemon.name).set(catchPokemon)
 
-                Log.e("Pokemon>>>", response.stats[1].stat.name)
-                Log.e("Pokemon>>>", response.stats[1].base_stat.toString())
-
-                Log.e("Pokemon>>>", response.stats[2].stat.name)
-                Log.e("Pokemon>>>", response.stats[2].base_stat.toString())
-
-                Log.e("Pokemon>>>", response.stats[3].stat.name)
-                Log.e("Pokemon>>>", response.stats[3].base_stat.toString())
-
-                Log.e("Pokemon>>>", response.stats[4].stat.name)
-                Log.e("Pokemon>>>", response.stats[4].base_stat.toString())
-
-                Log.e("Pokemon>>>", response.stats[5].stat.name)
-                Log.e("Pokemon>>>", response.stats[5].base_stat.toString())
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(applicationContext, "Pokemon atrapado", Toast.LENGTH_SHORT)
+                        .show()
+                }
 
             } catch (e: Exception) {
+                Log.e(">>>", e.toString())
                 // Si no encuentra un pokemon o el nombre es incorrecto
                 withContext(Dispatchers.Main) {
                     Toast.makeText(applicationContext, "Pokemon no encontrado", Toast.LENGTH_SHORT)
--- app/src/main/java/com/example/pokedex/Pokemon.kt
+++ app/src/main/java/com/example/pokedex/Pokemon.kt
@@ -6,10 +6,11 @@ data class Pokemon(
 
     var id: String = "",
     var name: String = "",
-    var attack: Number = 0,
-    var defense: Number = 0,
-    var specialAttack: Number = 0,
-    var specialDefense: Number = 0,
-    var speed: Number = 0,
+    var hp: Int = 0,
+    var attack: Int = 0,
+    var defense: Int = 0,
+    var specialAttack: Int = 0,
+    var specialDefense: Int = 0,
+    var speed: Int = 0,
     var sprite: String = ""
 ) : Serializable

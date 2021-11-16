00425aef592d9f96deac79091430b2e43561da1c
stban2703 <s.t.ban1401@gmail.com>
2021-11-16 02:37:56 -0500

    Open pokemon view

--- app/src/main/java/com/example/pokedex/PokemonActivity.kt
+++ app/src/main/java/com/example/pokedex/PokemonActivity.kt
@@ -2,10 +2,12 @@ package com.example.pokedex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.example.pokedex.databinding.ActivityPokemonBinding
 
 class PokemonActivity : AppCompatActivity() {
 
+    private lateinit var pokemon: Pokemon
     private lateinit var binding: ActivityPokemonBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,5 +15,16 @@ class PokemonActivity : AppCompatActivity() {
         binding = ActivityPokemonBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        pokemon = intent.extras?.getSerializable("pokemon") as Pokemon
+        Glide.with(this).load(pokemon.sprite).centerCrop().into(binding.pokemonImage)
+        binding.pokemonNameTV.text = pokemon.name.replaceFirstChar {
+            it.uppercase()
+        }
+        binding.hpTV.text = pokemon.hp.toString()
+        binding.attackTV.text = pokemon.attack.toString()
+        binding.defenseTV.text = pokemon.defense.toString()
+        binding.spAttackTV.text = pokemon.specialAttack.toString()
+        binding.spDefenseTV.text = pokemon.specialDefense.toString()
+        binding.speedTV.text = pokemon.speed.toString()
     }
 }
--- app/src/main/java/com/example/pokedex/PokemonView.kt
+++ app/src/main/java/com/example/pokedex/PokemonView.kt
@@ -1,8 +1,10 @@
 package com.example.pokedex
 
+import android.content.Intent
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 
 class PokemonView(itemView: View): RecyclerView.ViewHolder(itemView) {
@@ -10,5 +12,15 @@ class PokemonView(itemView: View): RecyclerView.ViewHolder(itemView) {
     var pokemon: Pokemon? = null
     val rowNameTV: TextView = itemView.findViewById(R.id.rowNameTV)
     val rowImage: ImageView = itemView.findViewById(R.id.rowImage)
+    val pokemonLayout: ConstraintLayout = itemView.findViewById(R.id.pokemonLayout)
+
+    init {
+        pokemonLayout.setOnClickListener {
+            val intent = Intent(pokemonLayout.context, PokemonActivity::class.java).apply {
+                putExtra("pokemon", pokemon)
+            }
+            pokemonLayout.context.startActivity(intent)
+        }
+    }
 
 }
